31c7335c4f2cfab8b0d341ab6f696f06acd4981a
j0saan97 <sampere39@gmail.com>
2026-05-17 17:37:33 +0200

    borramos el contenido de reractcomponets pa dejar la rama limpia

--- readme.txt
+++
@@ -1,5 +0,0 @@
-pip install simple-http-server
-cd <ruta_del_proyecto>
-python -m http.server
-
-https://www.deckofcardsapi.com/
--- src/js/valores.js
+++
@@ -1,6 +0,0 @@
-export default function valores() {
-    // Valores estándar para Texas Hold'em Poker
-    return ['A', '2', '3', '4', 
-            '5', '6', '7', '8', 
-            '9', '0', 'J', 'Q', 'K'];
-}
