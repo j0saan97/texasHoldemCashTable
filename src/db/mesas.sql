2a0cf1126a6485ed2b91f9ce246309a40f296a71
j0saan97 <sampere39@gmail.com>
2026-07-08 09:45:24 +0200

    chore: limpiar archivos mesa de cash de biblioteca_manos_dev

--- src/db/mesas.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE mesas(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    stake VARCHAR(10) UNIQUE NOT NULL,
-    descripcion VARCHAR(255) DEFAULT '-',
-    ciega_minima DECIMAL(10,4) NOT NULL,
-    ciega_maxima DECIMAL(10,4) NOT NULL,
-    ante DECIMAL(10,4) NOT NULL
-);
