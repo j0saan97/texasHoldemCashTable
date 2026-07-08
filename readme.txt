6579653d071afbef189b51d843d0855880ad03b0
j0saan97 <sampere39@gmail.com>
2026-07-08 09:46:02 +0200

    borramos to lo q no es la biblioteca

--- readme.txt
+++ readme.txt
@@ -1,5 +1,3 @@
-pip install simple-http-server
-cd <ruta_del_proyecto>
-python -m http.server
+
 
 https://www.deckofcardsapi.com/
--- src/js/palos.js
+++
@@ -1,4 +0,0 @@
-export default function palos(){
-    // Palos estándar para Texas Hold'em Poker
-    return ['H', 'D', 'C', 'S'];
-}
